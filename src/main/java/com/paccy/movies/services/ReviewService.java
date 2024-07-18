877a7364a1fb725ba65c680518f0cc6f227f16f6
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-18 13:08:39 +0200

    added mongoTemplate to update movie with reviews

--- src/main/java/com/paccy/movies/services/ReviewService.java
+++ src/main/java/com/paccy/movies/services/ReviewService.java
@@ -1,8 +1,12 @@
 package com.paccy.movies.services;
 
+import com.paccy.movies.documents.Movie;
 import com.paccy.movies.documents.Review;
 import com.paccy.movies.repositories.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +15,15 @@ public class ReviewService {
 
 
     private final ReviewRepository reviewRepository;
+    private final MongoTemplate mongoTemplate;
 
 
     public Review createReview(String reviewBody,String imdbId){
         Review review= new Review(reviewBody);
        reviewRepository.insert(review);
+       mongoTemplate.update(Movie.class)
+               .matching(Criteria.where("imdbIs").is(imdbId))
+               .apply(new Update().push("reviewIds").value(review));
+
     }
 }
