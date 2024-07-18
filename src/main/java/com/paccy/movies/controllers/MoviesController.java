5efe703149ba3add48cfcd47e30218108eca1682
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-18 10:35:24 +0200

    called getAllMovies method in the controller

--- src/main/java/com/paccy/movies/controllers/MoviesController.java
+++ src/main/java/com/paccy/movies/controllers/MoviesController.java
@@ -1,15 +1,23 @@
 package com.paccy.movies.controllers;
 
+import com.paccy.movies.documents.Movie;
+import com.paccy.movies.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
+@RequiredArgsConstructor
 public class MoviesController {
 
+    private final MovieService movieService;
     @GetMapping
-    public String allMovies(){
-        return  "All movies";
+    public ResponseEntity<List<Movie>> allMovies(){
+        return  ResponseEntity.ok(movieService.getAllMovies());
     }
 }
