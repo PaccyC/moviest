ba99f56538137ba135a568657311f2c6638c4e05
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-18 10:57:13 +0200

    added functionality to get movie by Id

--- src/main/java/com/paccy/movies/controllers/MoviesController.java
+++ src/main/java/com/paccy/movies/controllers/MoviesController.java
@@ -3,12 +3,15 @@ package com.paccy.movies.controllers;
 import com.paccy.movies.documents.Movie;
 import com.paccy.movies.services.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,5 +22,10 @@ public class MoviesController {
     @GetMapping
     public ResponseEntity<List<Movie>> allMovies(){
         return  ResponseEntity.ok(movieService.getAllMovies());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable ObjectId id){
+        return  ResponseEntity.ok(movieService.getMovieById(id));
     }
 }
--- src/main/java/com/paccy/movies/services/MovieService.java
+++ src/main/java/com/paccy/movies/services/MovieService.java
@@ -3,9 +3,11 @@ package com.paccy.movies.services;
 import com.paccy.movies.documents.Movie;
 import com.paccy.movies.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +17,10 @@ public class MovieService {
 
     public List<Movie> getAllMovies(){
         return  movieRepository.findAll();
+    }
+
+    public Optional<Movie> getMovieById(ObjectId id){
+        return  movieRepository.findById(id);
     }
 }
 
